464dd24f378a30c4b1fdfc59bf443c1ca48b4b95
Hilster Barbosa Santos <69377592+Hilster00@users.noreply.github.com>
2023-03-09 23:32:21 -0300

    Create SalvandoArquivoss.py

---
+++ ManipulacaoArquivos/SalvandoArquivoss.py
@@ -0,0 +1,20 @@
+#'w' para escrita, apagando todo o conteudo do arquivo
+arquivo=open("arquivo.txt","a")
+arquivo.write("palavra\n")
+arquivo.close()
+
+#'a' para escrita, adicionar texto ao final do arquivo
+arquivo=open("arquivo.txt","a")
+arquivo.write("palavra\n")
+arquivo.close()
+
+#'r' para leitura
+arquivo=open("arquivo.txt","r")
+teste=arquivo.read()
+arquivo.close()
+
+#'rb' para leitura de arquivos binarios
+arquivo=open("arquivo.txt","rb")
+teste=arquivo.read()
+arquivo.close()
+
